c9ab9ce2902dfc7952a41ba766e1d7142df6cf38
ben casalino & marcos moncivais <student@epicodus.com>
2015-08-06 11:57:43 -0700

    add content to app.php file

--- app/app.php
+++ app/app.php
@@ -1,3 +1,14 @@
-require_once __DIR__."/../vendor/autoload.php";
-require_once __DIR__."/../src/Task.php";
-  
+<?php
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+));
+
+
+
+$app->get("/", function() use ($app) {
+
+return $app['twig']->render('tasks.html.twig');
+
+}
+ ?> 
